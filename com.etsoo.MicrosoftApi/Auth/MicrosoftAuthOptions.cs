b3120fedad6436fa816673f6696b845e2c80e1e7
Garry Xiao <xz@etsoo.com>
2024-07-05 23:33:47 +1200

    Add AddMicrosoftAuthClient

--- com.etsoo.MicrosoftApi/Auth/MicrosoftAuthOptions.cs
+++ com.etsoo.MicrosoftApi/Auth/MicrosoftAuthOptions.cs
@@ -1,4 +1,7 @@
-﻿namespace com.etsoo.MicrosoftApi.Auth
+﻿using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+
+namespace com.etsoo.MicrosoftApi.Auth
 {
     /// <summary>
     /// Microsoft Auth options
@@ -10,26 +13,36 @@
         /// Authority, default is https://login.microsoftonline.com/common
         /// 'common' is the tenant value. Valid values are common, organizations, consumers, and tenant identifiers.
         /// </summary>
+        [Url]
         public string? Authority { get; set; }
 
         /// <summary>
         /// The Application (client) ID that the Microsoft Entra admin center – App registrations experience assigned to your app
         /// </summary>
-        public string ClientId { get; set; } = string.Empty!;
+        [Required]
+        public required string ClientId { get; set; }
 
         /// <summary>
         /// The application secret that you created in the app registration portal for your app
         /// </summary>
-        public string ClientSecret { get; set; } = string.Empty!;
+        [Required]
+        public required string ClientSecret { get; set; }
 
         /// <summary>
         /// Authorized redirect URIs for the server side application
         /// </summary>
+        [Url]
         public string? ServerRedirectUrl { get; set; }
 
         /// <summary>
         /// Authorized redirect URIs for the script side application
         /// </summary>
+        [Url]
         public string? ScriptRedirectUrl { get; set; }
+    }
+
+    [OptionsValidator]
+    public partial class ValidateMicrosoftAuthOptions : IValidateOptions<MicrosoftAuthOptions>
+    {
     }
 }
---
+++ com.etsoo.MicrosoftApi/MicrosoftApiServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using com.etsoo.MicrosoftApi.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace com.etsoo.MicrosoftApi
+{
+    /// <summary>
+    /// Microsoft API service collection extensions
+    /// 微软API服务集合扩展
+    /// </summary>
+    public static class MicrosoftApiServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add Microsoft auth client
+        /// 添加微软授权客户端
+        /// </summary>
+        /// <param name="services">Services</param>
+        /// <param name="configuration">configuration</param>
+        /// <returns>Services</returns>
+        public static IServiceCollection AddMicrosoftAuthClient(this IServiceCollection services, IConfigurationSection configuration)
+        {
+            services.AddSingleton<IValidateOptions<MicrosoftAuthOptions>, ValidateMicrosoftAuthOptions>();
+            services.AddOptions<MicrosoftAuthOptions>().Bind(configuration).ValidateOnStart();
+            services.AddHttpClient<IMicrosoftAuthClient, MicrosoftAuthClient>();
+            return services;
+        }
+    }
+}
